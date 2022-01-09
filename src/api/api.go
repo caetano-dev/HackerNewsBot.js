1f02749a0786652f299add91b05eb6aea66d9ba3
drull1000 <pcaetanop@protonmail.com>
2022-01-09 17:32:57 -0300

    Refactoring

--- src/api/api.go
+++ src/api/api.go
@@ -3,13 +3,12 @@ package api
 import (
 	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
-	"os"
 	"strings"
 
+	u "hackernewsbot/utils"
+
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
-	"github.com/joho/godotenv"
 )
 
 var relevantTopics = []string{"privacy", "hack", "linux", "golang", "hacker", "malware", "exploit", "leak", "CIA", "NSA", "hacked", "breaches", "breached", "security", "OSINT", "leaked"}
@@ -17,7 +16,7 @@ var newStoriesIDs = "https://hacker-news.firebaseio.com/v0/newstories.json?print
 var newsInfos = "https://hacker-news.firebaseio.com/v0/item/%d.json?print=pretty"
 var ids []int
 
-type News struct {
+type news struct {
 	Title string `json:"title"`
 	URL   string `json:"url"`
 }
@@ -41,37 +40,25 @@ func GetLatestNewsID() ([]int, error) {
 
 // FetchNews returns the news titles and urls
 func FetchNews(update tgbotapi.Update) {
-	error := godotenv.Load()
-	if error != nil {
-		log.Fatal("Error loading .env file")
-	}
+	bot, err := u.Login()
 
-	bot, error := tgbotapi.NewBotAPI(os.Getenv("TOKEN"))
-
-	if error != nil {
-		log.Panic(error)
-	}
+	u.HandleError(err)
 
 	response, err := GetLatestNewsID()
-	if err != nil {
-		fmt.Println(err)
-	}
+	u.HandleError(err)
 
-	news := News{}
+	news := news{}
 
 	for _, id := range response {
 		resp, err := http.Get(fmt.Sprintf(newsInfos, id))
-		if err != nil {
-			fmt.Println(err)
-		}
+		u.HandleError(err)
 		defer resp.Body.Close()
+
 		if resp.StatusCode != http.StatusOK {
 			fmt.Println(fmt.Sprintf("Error %s", resp.Status))
 		}
 		err = json.NewDecoder(resp.Body).Decode(&news)
-		if err != nil {
-			fmt.Println(err)
-		}
+		u.HandleError(err)
 		for _, topic := range relevantTopics {
 			if news.Title != "" && news.URL != "" {
 				if strings.Contains(strings.ToLower(news.Title), topic) {
--- src/main.go
+++ src/main.go
@@ -2,30 +2,19 @@ package main
 
 import (
 	"log"
-	"os"
 
 	a "hackernewsbot/api"
+	u "hackernewsbot/utils"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	error := godotenv.Load()
-	if error != nil {
-		log.Fatal("Error loading .env file")
-	}
-
-	bot, error := tgbotapi.NewBotAPI(os.Getenv("TOKEN"))
-	if error != nil {
-		log.Panic(error)
-	}
-
+	bot, error := u.Login()
+	u.HandleError(error)
 	log.Printf("Starting %s", bot.Self.UserName)
-
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
-
 	updates := bot.GetUpdatesChan(u)
 
 	for update := range updates {
