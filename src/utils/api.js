1b709c084f4de7a4b12a84bf31a37cfeeb372171
drull1000 <pcaetanop@protonmail.com>
2022-01-11 12:38:16 -0300

    Writing data to json

--- src/utils/api.js
+++ src/utils/api.js
@@ -9,7 +9,7 @@ var news = {
     news: []
 };
 
-const relevantTopics = ["privacy", "hack", "linux", "golang", "hacker", "malware", "exploit",
+const relevantTopics = ["privacy", "hack", "linux", "golang", "hacker", "the", "malware", "exploit",
 "leak", "CIA", "NSA", "hacked", "breaches", "breached", "security", "OSINT", "leaked", "GNUl", "free and open source","open source"]
 const newStoriesIDs = "https://hacker-news.firebaseio.com/v0/newstories.json?print=pretty"
 const newsInfos = "https://hacker-news.firebaseio.com/v0/item/"+newsID+".json?print=pretty"
@@ -17,17 +17,14 @@ const newsInfos = "https://hacker-news.firebaseio.com/v0/item/"+newsID+".json?pr
 const getLatestNewsIds = async () =>{
     try {
         const response = await axios.get(
-"https://hacker-news.firebaseio.com/v0/newstories.json?print=pretty"
+            newStoriesIDs
         );
         for (let i = 0; i < response.data.length; i++) {
             idsArray.push(response.data[i])
         }
-
         return idsArray
-        console.log(idsArray)
     } catch (error) {
-        console.log(error);
-        return "Sorry, got an error";
+        return error;
     }
 };
 
@@ -43,32 +40,25 @@ const fetchNewsInfo = async () => {
             );
             for (let j = 0; j < relevantTopics.length; j++) {
                 if (response.data.title.toLowerCase().includes(relevantTopics[j])) {
-                    console.log(response.data.title)
                     relevantNews.push(response.data)
                 }
             }
-
         }
 
-        console.log(relevantNews)
-        //append relevantNews to json
-        //     fs.writeFileSync('./relevantNews.json', JSON.stringify(relevantNews), 'utf8');
-
-        //write relevantNews to json
-        //      fs.writeFileSync('news.json', JSON.stringify(relevantNews))
+        fs.writeFileSync("./news.json", JSON.stringify(relevantNews));
 
         fs.readFile('news.json', 'utf8', function readFileCallback(err, data){
             if (err){
                 console.log(err);
             } else {
                 console.log(data);
-                news = JSON.parse(data); //now it an object
-                news.push(data); //add some data
-                json = JSON.stringify(relevantNews); //convert it back to json
+                news = JSON.parse(data); 
+                news.push(data); 
+                json = JSON.stringify(relevantNews); 
                 fs.writeFile('news.json', json, 'utf8', (err)=>{
                     if (err) throw err;
                     console.log('The file has been saved!');
-                }); // write it back 
+                }); 
             }}); 
 
         return relevantNews
