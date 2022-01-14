a6f474ea86b38f0057bbd252187227d8b7c37059
drull1000 <pcaetanop@protonmail.com>
2022-01-14 09:18:53 -0300

    Filtering news properly

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ const TelegramBot = require('node-telegram-bot-api');
 const token = process.env.TOKEN
 const bot = new TelegramBot(token, {polling: true});
 const { getLatestNewsIds } = require("./utils/api");
-const { fetchNewsTitleAndURL } = require("./utils/api");
+const { fetchRelevantNews } = require("./utils/api");
+const { checkIfNewsIsInJson } = require("./utils/api");
 
 bot.onText(/\/start/, async (msg) => {
     const chatId = msg.chat.id;
-    const latestNews = await fetchNewsTitleAndURL();
+//    const latestNews = await fetchRelevantNews();
+    const latestNews = await checkIfNewsIsInJson();
     for (let i = 0; i < latestNews.length; i++) {
         const news = latestNews[i];
         const newsText = `${news.title}
--- src/utils/api.js
+++ src/utils/api.js
@@ -2,13 +2,15 @@ const fs = require('fs');
 const axios = require("axios").default;
 
 let newsID
-let idsArray = []
 
 const relevantTopics = ["privacy", "a", "the", "hack", "linux", "golang", "hacker", "malware", "exploit",
 "leak", "CIA", "NSA", "hacked", "breaches", "breached", "security", "OSINT", "leaked", "GNUl", "free and open source","open source"]
 const newsIDsLink = "https://hacker-news.firebaseio.com/v0/newstories.json?print=pretty"
 
+//get the latest news id
 const getLatestNewsIds = async () =>{
+
+    let idsArray = []
     try {
         const response = await axios.get(
             newsIDsLink
@@ -22,29 +24,8 @@ const getLatestNewsIds = async () =>{
     }
 };
 
-const fetchNewsTitleAndURL = async () => {
-    try {
-        console.log("Fetching news...")
-        const newsID = await getLatestNewsIds()
-        let news = []
-        //        for (let i = 0; i < newsID.length; i++) {
-        for (let i = 0; i < 5; i++) {
-            const newsLink = "https://hacker-news.firebaseio.com/v0/item/" + newsID[i] + ".json?print=pretty"
-            const response = await axios.get(
-                newsLink
-            );
-            news.push(response.data)
-        }
-        let relevantNews = checkIfNewsIsRelevant(news)
-        console.log(relevantNews)
-
-        return relevantNews
-    } catch (error) {
-        return error;
-    }
-};
-
-function checkIfNewsIsRelevant(news) {
+//check if the news is relevant
+const checkIfNewsIsRelevant = async (news) => {
     console.log("Checking if news is relevant...")
     let relevantNews = []
     for (let i = 0; i < news.length; i++) {
@@ -61,8 +42,56 @@ function checkIfNewsIsRelevant(news) {
     return relevantNews
 }
 
+//get the relevant news
+const fetchRelevantNews = async () => {
+    try {
+        console.log("Fetching news...")
+        const newsID = await getLatestNewsIds()
+        let news = []
+        //        for (let i = 0; i < newsID.length; i++) {
+        for (let i = 0; i < 5; i++) {
+            const newsLink = "https://hacker-news.firebaseio.com/v0/item/" + newsID[i] + ".json?print=pretty"
+            const response = await axios.get(
+                newsLink
+            );
+            news.push(response.data)
+        }
+        let relevantNews = await checkIfNewsIsRelevant(news)
+        //console.log(relevantNews)
+
+        return relevantNews
+    } catch (error) {
+        return error;
+    }
+};
+
+
+//check if the relevant news are already in the json file
+const checkIfNewsIsInJson = async () => {
+    let relevantNews = await fetchRelevantNews()
+    let newsToBeAdded = []
+    console.log("Checking if news is in json...")
+    let newsInJson = fs.readFileSync('./news.json', 'utf8')
+    console.log("news is json are " + newsInJson)
+
+    for (let i = 0; i < relevantNews.length; i++) {
+        //open the news.json file and check if the news is already in the file
+        //if not, add it to the newsToBeAdded array
+        //if yes, do nothing
+
+        if (newsInJson.includes(relevantNews[i].title)) {
+            console.log(relevantNews[i].title + " is already in the json file")
+        } else {
+            newsToBeAdded.push(relevantNews[i])
+        }
+    }
+    return newsToBeAdded
+}
+
+
 module.exports = {
-    fetchNewsTitleAndURL,
+    fetchRelevantNews,
+    checkIfNewsIsInJson,
     getLatestNewsIds
 };
 
